741cd555c70515f76fcf155a0cd2bf106aacb38a
dataprojectobenk <dataprojectobenk@gmail.com>
2023-09-19 22:21:35 +0700

    offcanvas

--- dashboard_v2/dashboard_v2.js
+++ dashboard_v2/dashboard_v2.js
@@ -1,4 +1,19 @@
 let btn = document.querySelector('#content button.navbar-toggler')
-btn.onclick =()=>{
+const bg_blur = document.createElement('div')
+let box = document.getElementById('content')
+bg_blur.classList.add('blur', 'd-none')
+box.appendChild(bg_blur)
+btn.onclick = () => {
     document.getElementById('wrapper').classList.toggle('show-menu')
+    bg_blur.classList.toggle('d-none', false)
 }
+bg_blur.onclick = () => {
+    document.getElementById('wrapper').classList.toggle('show-menu', false)
+    bg_blur.classList.toggle('d-none')
+}
+document.querySelectorAll('#sidebar a.nav-link').forEach(el => {
+    el.addEventListener('click', ()=>{
+        document.getElementById('wrapper').classList.toggle('show-menu', false)
+        bg_blur.classList.toggle('d-none')
+    })
+})
